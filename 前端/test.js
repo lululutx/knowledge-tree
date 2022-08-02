26aaf1e059f95100ae23af2b930bef7dfc003e27
路路路同学 <386334180@qq.com>
2022-08-02 21:47:12 +0800

    闭包，原型链，js继承

--- 前端/test.js
+++ 前端/test.js
@@ -1,19 +1,10 @@
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-
-console.log(animals.slice(2));
-// expected output: Array ["camel", "duck", "elephant"]
-
-console.log(animals.slice(2, 4));
-// expected output: Array ["camel", "duck"]
-
-console.log(animals.slice(1, 5));
-// expected output: Array ["bison", "camel", "duck", "elephant"]
-
-console.log(animals.slice(-2));
-// expected output: Array ["duck", "elephant"]
-
-console.log(animals.slice(2, -1));
-// expected output: Array ["camel", "duck"]
-
-console.log(animals.slice());
-// expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
+function Parent() {
+  this.age = 18
+}
+function Child() {
+  Parent.call(this)
+  this.name = '张三'
+}
+Child.prototype = new Parent()
+let o4 = new Child()
+console.log(o4, o4.age)//Parent { age: 18, name: '张三' } 18
